8d285cf1ce528e889958c08498bb7595d0a3d520
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-01-27 15:20:27 +0900

    0.7.4 Model의 areContentsTheSame에서 ===을 ==으로 수정하여 클래스의 내용을 제대로 비교할 수 있도록 수정 Model에 equals를 정의하여 areContentsTheSame에서 ==을 사용할 수 있도록 함

--- app/src/main/java/com/example/YUmarket/model/Model.kt
+++ app/src/main/java/com/example/YUmarket/model/Model.kt
@@ -18,17 +18,41 @@ abstract class Model(
     open fun isTheSame(item: Model) =
         this.id == item.id && this.type == item.type
 
+
     companion object {
-        val DIFF_CALLBACK = object: DiffUtil.ItemCallback<Model>() {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Model>() {
             override fun areItemsTheSame(oldItem: Model, newItem: Model): Boolean {
                 return oldItem.isTheSame(newItem)
             }
 
-            @SuppressLint("DiffUtilEquals")
             override fun areContentsTheSame(oldItem: Model, newItem: Model): Boolean {
-                return oldItem === newItem
+                return oldItem == newItem
             }
 
         }
+    }
+
+
+    /**
+     * [DIFF_CALLBACK]의 areContentsTheSame에서 사용할 equals
+     * equals가 없으면 areContentsTheSame에 @SuppressLint("DiffUtilEquals")를 붙여야한다.
+     * Model 클래스를 비교할 일이 없으므로 Annotation을 붙여도 상관없다.
+     */
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Model
+
+        if (id != other.id) return false
+        if (type != other.type) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + type.hashCode()
+        return result
     }
 }
