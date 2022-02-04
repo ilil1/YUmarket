9a000ee0b0f7d471001f707265f5a07025c0c817
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-02-04 17:28:27 +0900

    0.7.6 ViewHolderMapper에서 type이 HOME_MAIN_ITEM_CELL일 경우 NewSaleItemViewHolder를 생성하여 반환하도록 함

--- app/src/main/java/com/example/YUmarket/util/mapper/ViewHolderMapper.kt
+++ app/src/main/java/com/example/YUmarket/util/mapper/ViewHolderMapper.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.example.YUmarket.databinding.ViewholderHomeItemBinding
 import com.example.YUmarket.databinding.ViewholderNearbyMarketBinding
+import com.example.YUmarket.databinding.ViewholderNewSaleItemBinding
 import com.example.YUmarket.databinding.ViewholderTownMarketBinding
 import com.example.YUmarket.model.CellType
 import com.example.YUmarket.model.Model
@@ -13,6 +14,7 @@ import com.example.YUmarket.widget.adapter.viewholder.ModelViewHolder
 import com.example.YUmarket.widget.adapter.viewholder.home.HomeItemModelViewHolder
 import com.example.YUmarket.widget.adapter.viewholder.home.TownMarketViewHolder
 import com.example.YUmarket.widget.adapter.viewholder.homemain.NearbyMarketViewHolder
+import com.example.YUmarket.widget.adapter.viewholder.homemain.NewSaleItemViewHolder
 
 object ViewHolderMapper {
     @Suppress("UNCHECKED_CAST")
@@ -26,11 +28,18 @@ object ViewHolderMapper {
         val inflater = LayoutInflater.from(parent.context)
 
         return when (type) {
-            CellType.HOME_CELL -> {
+            CellType.HOME_MAIN_MARKET_CELL -> {
                 NearbyMarketViewHolder(
                     ViewholderNearbyMarketBinding.inflate(inflater),
                     viewModel,
                     resourcesProvider
+                )
+            }
+
+            CellType.HOME_MAIN_ITEM_CELL -> {
+                NewSaleItemViewHolder(
+                    ViewholderNewSaleItemBinding.inflate(inflater),
+                    viewModel, resourcesProvider
                 )
             }
 
