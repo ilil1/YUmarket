6753a11af1a756293b5aac19489078a6d4da4c5e
ErroredPasta <ForeignServices3132@pm.me>
2022-03-24 16:14:50 +0900

    0.10.0 appModule에 추가된 ViewModel과 Repository를 추가 ViewModel은 LikeListViewModel<LikeMarketModel>과 LikeListViewModel<LikeItemModel>을 추가하여 LikeCategory를 이용하여 구분되도록 함 Repository는 DefaultLikeMarketRepository와 DefaultLikeItemRepository를 추가하여 String을 이용하여 구분되도록 함

--- app/src/main/java/com/example/YUmarket/di/appModule.kt
+++ app/src/main/java/com/example/YUmarket/di/appModule.kt
@@ -6,10 +6,17 @@ import com.example.YUmarket.data.repository.map.DefaultMapRepository
 import com.example.YUmarket.data.repository.map.MapRepository
 import com.example.YUmarket.data.repository.home.DefaultHomeRepository
 import com.example.YUmarket.data.repository.home.HomeRepository
+import com.example.YUmarket.data.repository.like.DefaultLikeItemRepository
+import com.example.YUmarket.data.repository.like.DefaultLikeMarketRepository
+import com.example.YUmarket.data.repository.like.LikeListRepository
 import com.example.YUmarket.model.homelist.category.HomeListCategory
+import com.example.YUmarket.model.like.LikeCategory
+import com.example.YUmarket.model.like.LikeItemModel
+import com.example.YUmarket.model.like.LikeMarketModel
 import com.example.YUmarket.screen.MainViewModel
 import com.example.YUmarket.screen.home.homelist.HomeListViewModel
 import com.example.YUmarket.screen.home.homemain.HomeMainViewModel
+import com.example.YUmarket.screen.like.LikeListViewModel
 import com.example.YUmarket.screen.orderlist.OrderListViewModel
 import com.example.YUmarket.util.provider.DefaultResourcesProvider
 import com.example.YUmarket.util.provider.ResourcesProvider
@@ -27,6 +34,22 @@ val appModule = module {
 
     viewModel { MainViewModel(get(), get()) }
     viewModel { OrderListViewModel() }
+
+    viewModel(named(LikeCategory.MARKET)) {
+        LikeListViewModel<LikeMarketModel>(
+            get(
+                qualifier = named("likeMarketRepository")
+            )
+        )
+    }
+
+    viewModel(named(LikeCategory.ITEM)) {
+        LikeListViewModel<LikeItemModel>(
+            get(
+                qualifier = named("likeItemRepository")
+            )
+        )
+    }
 
     single<HomeRepository> { DefaultHomeRepository() }
 
@@ -51,4 +74,12 @@ val appModule = module {
     single { provideLikeMarketDao(get()) }
 
     single<BasketRepository> { DefaultBasketRepository(get(), get()) }
+
+    single<LikeListRepository<LikeMarketModel>>(named("likeMarketRepository")) {
+        DefaultLikeMarketRepository(get(), get())
+    }
+
+    single<LikeListRepository<LikeItemModel>>(named("likeItemRepository")) {
+        DefaultLikeItemRepository(get(), get())
+    }
 }
