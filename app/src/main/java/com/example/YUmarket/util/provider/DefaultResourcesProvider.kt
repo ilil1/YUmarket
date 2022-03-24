1149fc072b9cfad7da68cf4cc4bebdcdf3fd499c
ErroredPasta <ForeignServices3132@pm.me>
2022-03-24 16:18:19 +0900

    0.10.1 DefaultResourcesProvider의 getColorStateList가 Stack Overflow가 되는 것을 수정

--- app/src/main/java/com/example/YUmarket/util/provider/DefaultResourcesProvider.kt
+++ app/src/main/java/com/example/YUmarket/util/provider/DefaultResourcesProvider.kt
@@ -3,6 +3,7 @@ package com.example.YUmarket.util.provider
 import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.drawable.Drawable
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
 
 /**
@@ -22,7 +23,7 @@ class DefaultResourcesProvider(
 
     override fun getColor(resId: Int): Int = ContextCompat.getColor(context, resId)
 
-    override fun getColorStateList(resId: Int): ColorStateList = getColorStateList(resId)
+    override fun getColorStateList(resId: Int): ColorStateList = AppCompatResources.getColorStateList(context, resId)
 
-    override fun getDrawable(resId: Int): Drawable? = context.getDrawable(resId)
+    override fun getDrawable(resId: Int): Drawable? = AppCompatResources.getDrawable(context, resId)
 }
