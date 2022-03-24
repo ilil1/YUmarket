69a3b56dd02d8004b053edde503ce75603dc9de1
ErroredPasta <ForeignServices3132@pm.me>
2022-03-24 16:11:56 +0900

    0.10.0 ViewHolderMapper에 LikeMarketViewHolder와 LikeItemViewHolder 추가

--- app/src/main/java/com/example/YUmarket/util/mapper/ViewHolderMapper.kt
+++ app/src/main/java/com/example/YUmarket/util/mapper/ViewHolderMapper.kt
@@ -2,19 +2,21 @@ package com.example.YUmarket.util.mapper
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import com.example.YUmarket.databinding.ViewholderHomeItemBinding
-import com.example.YUmarket.databinding.ViewholderNearbyMarketBinding
-import com.example.YUmarket.databinding.ViewholderNewSaleItemBinding
-import com.example.YUmarket.databinding.ViewholderTownMarketBinding
+import com.example.YUmarket.databinding.*
 import com.example.YUmarket.model.CellType
 import com.example.YUmarket.model.Model
+import com.example.YUmarket.model.like.LikeItemModel
+import com.example.YUmarket.model.like.LikeMarketModel
 import com.example.YUmarket.screen.base.BaseViewModel
+import com.example.YUmarket.screen.like.LikeListViewModel
 import com.example.YUmarket.util.provider.ResourcesProvider
 import com.example.YUmarket.widget.adapter.viewholder.ModelViewHolder
 import com.example.YUmarket.widget.adapter.viewholder.home.HomeItemModelViewHolder
 import com.example.YUmarket.widget.adapter.viewholder.home.TownMarketViewHolder
 import com.example.YUmarket.widget.adapter.viewholder.homemain.NearbyMarketViewHolder
 import com.example.YUmarket.widget.adapter.viewholder.homemain.NewSaleItemViewHolder
+import com.example.YUmarket.widget.adapter.viewholder.like.LikeItemViewHolder
+import com.example.YUmarket.widget.adapter.viewholder.like.LikeMarketViewHolder
 
 object ViewHolderMapper {
     @Suppress("UNCHECKED_CAST")
@@ -28,36 +30,46 @@ object ViewHolderMapper {
         val inflater = LayoutInflater.from(parent.context)
 
         return when (type) {
-            CellType.HOME_MAIN_MARKET_CELL -> {
-                NearbyMarketViewHolder(
-                    ViewholderNearbyMarketBinding.inflate(inflater),
-                    viewModel,
-                    resourcesProvider
-                )
-            }
+            CellType.HOME_MAIN_MARKET_CELL -> NearbyMarketViewHolder(
+                ViewholderNearbyMarketBinding.inflate(inflater),
+                viewModel,
+                resourcesProvider
+            )
 
-            CellType.HOME_MAIN_ITEM_CELL -> {
-                NewSaleItemViewHolder(
-                    ViewholderNewSaleItemBinding.inflate(inflater),
-                    viewModel, resourcesProvider
-                )
-            }
 
-            CellType.HOME_TOWN_MARKET_CELL -> {
-                TownMarketViewHolder(
-                    ViewholderTownMarketBinding.inflate(inflater),
-                    viewModel,
-                    resourcesProvider
-                )
-            }
+            CellType.HOME_MAIN_ITEM_CELL -> NewSaleItemViewHolder(
+                ViewholderNewSaleItemBinding.inflate(inflater),
+                viewModel,
+                resourcesProvider
+            )
 
-            CellType.HOME_ITEM_CELL -> {
-                HomeItemModelViewHolder(
-                    ViewholderHomeItemBinding.inflate(inflater),
-                    viewModel,
-                    resourcesProvider
-                )
-            }
+
+            CellType.HOME_TOWN_MARKET_CELL -> TownMarketViewHolder(
+                ViewholderTownMarketBinding.inflate(inflater),
+                viewModel,
+                resourcesProvider
+            )
+
+
+            CellType.HOME_ITEM_CELL -> HomeItemModelViewHolder(
+                ViewholderHomeItemBinding.inflate(inflater),
+                viewModel,
+                resourcesProvider
+            )
+
+
+            CellType.LIKE_MARKET_CELL -> LikeMarketViewHolder(
+                ViewholderLikeMarketListBinding.inflate(inflater),
+                viewModel as LikeListViewModel<LikeMarketModel>,
+                resourcesProvider
+            )
+
+            CellType.LIKE_ITEM_CELL -> LikeItemViewHolder(
+                ViewholderLikeItemListBinding.inflate(inflater),
+                viewModel as LikeListViewModel<LikeItemModel>,
+                resourcesProvider
+            )
+
         } as ModelViewHolder<M>
 
     }
