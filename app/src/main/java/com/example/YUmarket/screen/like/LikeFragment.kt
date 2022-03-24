0bbff0aef3b4db09d46bcf6c603f37e780fc5dff
ErroredPasta <ForeignServices3132@pm.me>
2022-03-24 14:54:00 +0900

    0.10.0 LikeFragment가 ViewPager를 이용하여 LikeListFragment를 포함하도록 구현

--- app/src/main/java/com/example/YUmarket/screen/like/LikeFragment.kt
+++ app/src/main/java/com/example/YUmarket/screen/like/LikeFragment.kt
@@ -1,11 +1,41 @@
 package com.example.YUmarket.screen.like
 
 import com.example.YUmarket.databinding.FragmentLikeBinding
+import com.example.YUmarket.model.like.LikeCategory
 import com.example.YUmarket.screen.base.BaseFragment
+import com.example.YUmarket.widget.adapter.ListFragmentPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class LikeFragment
     : BaseFragment<FragmentLikeBinding>() {
 
+    private lateinit var viewPagerAdapter: ListFragmentPagerAdapter
+
     override fun getViewBinding(): FragmentLikeBinding =
         FragmentLikeBinding.inflate(layoutInflater)
+
+    override fun initViews() {
+        super.initViews()
+        if (!::viewPagerAdapter.isInitialized) initViewPager()
+    }
+
+    private fun initViewPager() = with(binding) {
+        val likeCategories = LikeCategory.values()
+
+        val likeListFragments = likeCategories.map {
+            LikeListFragment.newInstance(it)
+        }
+
+        viewPagerAdapter = ListFragmentPagerAdapter(
+            this@LikeFragment,
+            fragmentList = likeListFragments
+        )
+
+        likeFragmentViewPager.adapter = viewPagerAdapter
+        likeFragmentViewPager.offscreenPageLimit = likeCategories.size
+
+        TabLayoutMediator(likeFragmentTabLayout, likeFragmentViewPager) { tab, position ->
+            tab.setText(likeCategories[position].likeCategoryId)
+        }.attach()
+    }
 }
