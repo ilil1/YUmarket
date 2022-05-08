27773dc15ad26b76aeaa3ac26c074349908e9907
ErroredPasta <ForeignServices3132@pm.me>
2022-05-08 18:08:06 +0900

    0.11.0 Package 이름 변경

--- app/src/androidTest/java/com/example/myapplication23/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/myapplication23/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication23
+package com.example.YUmarket
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.myapplication23", appContext.packageName)
+        assertEquals("com.example.YUmarket", appContext.packageName)
     }
 }
--- app/src/test/java/com/example/myapplication23/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication23/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication23
+package com.example.YUmarket
 
 import org.junit.Test
 
